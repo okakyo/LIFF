e6c9d05055da5ad55eca592bf2adac701f9cb886
Kyohei Oka <0622okakyo@gmail.com>
2019-01-21 18:17:29 +0900

    '練習中'

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import uuid
 import base64
 from PIL import Image
 import warnings
-
 warnings.simplefilter('error', Image.DecompressionBombWarning)
 
 app = Flask(__name__)
@@ -13,10 +12,7 @@ bootstrap = Bootstrap(app)
 
 @app.route('/')
 def do_get():
-        return render_template('index.html')
-
-
+    return render_template('index.html')
 if __name__ == '__main__':
-        app.debug = True
-        app.run()
-            
+    app.debug = True
+    app.run()

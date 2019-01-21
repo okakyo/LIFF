82ccb9ad2b9c91cea8f7e9b60b392072da385a56
Kyohei Oka <0622okakyo@gmail.com>
2019-01-21 18:27:14 +0900

    '練習中'

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask, request, render_template, make_response
+from flask import Flask, request, render_template
 from flask_bootstrap import Bootstrap
 import os
-
+import uuid
 import base64
+from PIL import Image
 import warnings
+
 warnings.simplefilter('error', Image.DecompressionBombWarning)
 
 app = Flask(__name__)
@@ -11,7 +13,9 @@ bootstrap = Bootstrap(app)
 
 @app.route('/')
 def do_get():
-    return render_template('index.html')
+        return render_template('index.html')
+
 if __name__ == '__main__':
-    app.debug = True
-    app.run()
+        app.debug = True
+        app.run()
+            

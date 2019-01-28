3e88332406c5c60d79614a9fe4817643b524518e
Kyohei Oka <0622okakyo@gmail.com>
2019-01-28 13:36:49 +0900

    'エラー編集'

--- app.py
+++ app.py
@@ -30,9 +30,9 @@ def AnswerText(text):
     if '入部' in text:
         answer+='こちらから登録をお願いします。{}\n'.format(url[1])
     if '質問' in text:
-        answer+='質問はこちらからお願いします。\n'.format(url[0])
+        answer+='質問はこちらからお願いします。{}\n'.format(url[0])
     if 'DENX' in text:
-        answer+='DENXはこちら。{}\n'.format(+url[3])
+        answer+='DENXはこちら。{}\n'.format(url[3])
     if 'ホーム' in text:
         answer+='ホームページはこちらからお願いします。{}\n'.format(url[2])
     else:

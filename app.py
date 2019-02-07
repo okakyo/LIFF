dc6fedfb3b610e605f297368620b1cc175b24b1a
Kyohei Oka <0622okakyo@gmail.com>
2019-02-07 20:47:07 +0900

    'テンプレートメッセージ追加'

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ def handle_message(event):
 
     elif(event.message.text=='リスト'):
          buttons_template = ButtonsTemplate(
-            thumbnail_image_url="static\img\denx.png",title='My buttons sample',text='Hello, my buttons',actions=[
+            thumbnail_image_url="/static/img/denx.png",title='My buttons sample',text='Hello, my buttons',actions=[
                 URIAction(label='ホームページ', uri='https://denx.jp'),
             ])
          template_message = TemplateSendMessage(alt_text='Buttons alt text', template=buttons_template)

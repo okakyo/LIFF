fb5b899ef0bbea0115df08915991416b9bbb5d64
Kyohei Oka <0622okakyo@gmail.com>
2019-02-07 20:38:47 +0900

    'テンプレートメッセージ追加'

--- app.py
+++ app.py
@@ -77,10 +77,8 @@ def handle_message(event):
     elif(event.message.text=='リスト'):
          buttons_template = ButtonsTemplate(
             title='My buttons sample', text='Hello, my buttons', actions=[
-                URIAction(label='Go to line.me', uri='https://line.me'),
-                PostbackAction(label='ping', data='ping'),
-                PostbackAction(label='ping with text', data='ping', text='ping'),
-                MessageAction(label='Translate Rice', text='米')
+                URIAction(label='ホームページ', uri='https://denx.jp'),
+                
             ])
          template_message = TemplateSendMessage(alt_text='Buttons alt text', template=buttons_template)
          line_bot_api.reply_message(event.reply_token, template_message)

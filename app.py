664e26698ddd70b8ec480c178f48d1a770a26801
Kyohei Oka <0622okakyo@gmail.com>
2019-02-07 20:35:57 +0900

    'テンプレートメッセージ追加'

--- app.py
+++ app.py
@@ -75,10 +75,16 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token,messages=orders)
 
     elif(event.message.text=='リスト'):
-        button_template=ButtonsTemplate(title='DENX',actions=[URIAction(label='ホームページ',uri='https://denx.jp')])
-        template=TemplateSendMessage(alt_text='Hello',template=button_template)
-        line_bot_api.reply_message(event.reply_token,template)
-        
+         buttons_template = ButtonsTemplate(
+            title='My buttons sample', text='Hello, my buttons', actions=[
+                URIAction(label='Go to line.me', uri='https://line.me'),
+                PostbackAction(label='ping', data='ping'),
+                PostbackAction(label='ping with text', data='ping', text='ping'),
+                MessageAction(label='Translate Rice', text='米')
+            ])
+        template_message = TemplateSendMessage(
+            alt_text='Buttons alt text', template=buttons_template)
+        line_bot_api.reply_message(event.reply_token, template_message)
     else:
         line_bot_api.reply_message(event.reply_token,TextMessage(text='DENXに用がある方は、「オーダー」と入力してください。'))
 

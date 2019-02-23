26faf075de155919b2687388c09cbdc77fc1380c
Kyohei Oka <0622okakyo@gmail.com>
2019-02-23 17:25:54 +0900

    'LIFF機能追加（したい）'

---
+++ static/js/liff.js
@@ -0,0 +1,23 @@
+const Device_UUID='';
+const Characteristic_UUID='';
+
+
+window.onload = ()=>{
+    initializeApp();
+};
+
+function handlerApp() {
+    return;
+};
+function initializeApp() {
+    return;
+};
+function handlerApp() {
+    return;
+};
+function handlerApp() {
+    return;
+};
+function handlerApp() {
+    return;
+};
